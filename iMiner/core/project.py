e53162828b27f21b1a001893242a7837d1547f88
Yingze Wang <ericwangyz@pku.edu.cn>
2022-10-25 00:38:23 -0700

    update (project): add more functions

--- iMiner/core/project.py
+++ iMiner/core/project.py
@@ -1,5 +1,5 @@
 '''
-Author: Jie Li
+Author: Jie Li, Eric Wang
 Date Created: Oct 21, 2022
 
 This file defines the BaseProject class with associated logic for project initialization
@@ -8,7 +8,10 @@ The protein is supposed to be a pdb file and the ligand is supposed to be a sdf 
 
 import os
 import shutil
+from pathlib import Path
+from collections import OrderedDict
 from rdkit.Chem import MolFromSmiles, AddHs, AllChem, SDWriter
+
 
 class BaseProject:
     def __init__(self, project_name, project_path=None) -> None:
@@ -33,38 +36,46 @@ class BaseProject:
 
         # prepare protein (with binding sites) mapping dicts that map names to the corresponding file paths
         # because ligands do not have names, the ligand paths are saved as a list
-        self.proteins = {}
-        self.binding_sites = {}
-        self.ligands = []
+        self.proteins = OrderedDict()
+        self.binding_sites = OrderedDict()
+        self.ligands = OrderedDict()
 
         # provide a cache for processed protein pdb files (in case multiple binding sites are defined for the same protein)
         self._protein_processed_cache = set()
 
-    def add_protein(self, name, protein_file_path, binding_site=None):
+    def add_protein(self, protein_file_path, name=None, preprocess=False, binding_site=None):
         '''
         Add a protein with corresponding binding site definition to the project
 
-        :param name: the name of the protein + pocket
         :param protein_file_path: path to the protein file
+        :param name: the name of the protein + pocket, default None
+        :param preprocess: if preprocess the protein pdb with `pdbfixer`
         :param binding_site: (xmin, ymin, zmin, xmax, ymax, zmax), the binding site definition
-        :type name: str
         :type protein_file_path: str
+        :type name: str
+        :type preprocess: bool
         :type binding_site: tuple
         '''
         self.binding_sites[name] = binding_site
-        # preprocess the protein file and save the clean file to project_path/proteins
-        # if the protein has been processed before, skip the preprocessing step
+        name = str(len(self.proteins.items) + 1) if name is None else name
+        protein_path = os.path.join(self.project_path, "proteins", f"{name}.pdb")
+        if preprocess:
+            raise NotImplementedError()
+        else:
+            shutil.copyfile(protein_file_path, protein_path)
+        self.proteins[name] = protein_path
 
-    def add_ligand(self, smiles_or_path, format='inferred'):
+    def add_ligand(self, smiles_or_path, name=None, format='inferred'):
         '''
         Add a ligand to the project
 
+        :param name: Name of the ligand
         :param smiles_or_path: Either the smiles string of the ligand or the path to the ligand file
         :param format: One of ['inferred', 'smiles', 'sdf', 'pdb']. If format is 'inferred', the format will be inferred from the file extension
+        :type name: str
         :type smiles_or_path: str
         :type format: str
         '''
-
         # decide the file format
         if format == 'inferred':
             if smiles_or_path.endswith('.sdf'):
@@ -75,18 +86,66 @@ class BaseProject:
                 format = 'smiles'
 
         # get ligand name according to its index in the current project
-        ligand_name = f'{len(self.ligands) + 1}.sdf'
+        if name is None:
+            name = str(len(self.ligands.items()) + 1)
+        ligand_name = f"{name}.sdf"
+        ligand_path = os.path.join(self.project_path, 'ligands', ligand_name)
 
         # process the ligand file according to the format
         if format == 'smiles':
-            self.ligands.append(self._process_smiles(smiles_or_path, os.path.join(self.project_path, 'ligands', ligand_name)))
+            self._process_smiles(smiles_or_path, ligand_path)
         elif format == 'sdf':
             # directly copy the sdf file to the corresponding position
-            shutil.copy(smiles_or_path, os.path.join(self.project_path, 'ligands', ligand_name))
-            self.ligands.append(os.path.join(self.project_path, 'ligands', ligand_name))
+            shutil.copy(smiles_or_path, ligand_path)
         elif format == 'pdb':
-            self.ligands.append(self._process_pdb(smiles_or_path, os.path.join(self.project_path, 'ligands', ligand_name)))
+            self._process_pdb(smiles_or_path, ligand_path)
+        
+        self.ligands[name] = ligand_path
+    
+    def get_ligand_with_name(self, name) -> Path:
+        """
+        Get ligand file path with its name
 
+        :param name: Name of the query ligand
+        :type name: str
+        :return: Path of the query ligand
+        :rtype: pathlib.Path
+        """
+        return Path(self.ligands[name]).resolve()
+    
+    def get_ligand_with_index(self, idx) -> Path:
+        """
+        Get ligand file path with its index
+
+        :param idx: Index of the query ligand
+        :type idx: int
+        :return: Path of the query ligand
+        :rtype: pathlib.Path
+        """
+        return Path(self.ligands.items()[idx][-1]).resolve()
+    
+    def get_protein_with_name(self, name) -> Path:
+        """
+        Get ligand file path with its name
+
+        :param name: Name of the query ligand
+        :type name: str
+        :return: Path of the query ligand
+        :rtype: pathlib.Path
+        """
+        return Path(self.proteins[name]).resolve()
+    
+    def get_protein_with_index(self, idx) -> Path:
+        """
+        Get ligand file path with its index
+
+        :param idx: Index of the query ligand
+        :type idx: int
+        :return: Path of the query ligand
+        :rtype: pathlib.Path
+        """
+        return Path(self.proteins.items()[idx][-1]).resolve()
+        
     def _process_smiles(self, smiles, save_path):
         '''
         Process a smiles string and save the corresponding ligand file to project_path/ligands
