7daf310734ce87e989a039cb643895163fc57649
Jerry <jerry-li1996@hotmail.com>
2022-11-03 17:18:16 -0700

    update utility functions

--- iMiner/utils.py
+++ iMiner/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import random
+import datetime
+import sys
 
 def dist_mat(crd1: np.ndarray, crd2: np.ndarray) -> np.ndarray:
     '''
@@ -22,3 +24,27 @@ def dist_mat(crd1: np.ndarray, crd2: np.ndarray) -> np.ndarray:
     expand_crd2 = np.broadcast_to(crd2, (crd1.shape[0], crd2.shape[0], 3))
     dist_mat = np.linalg.norm(expand_crd1 - expand_crd2, ord=2, axis=-1)
     return dist_mat
+
+
+def hash_str(string) -> str:
+    '''
+    Generate a unique hash code for a string
+    '''
+    hashcode = hex(hash(string) % ((sys.maxsize + 1) * 2)).replace("0x", "")
+    return hashcode
+
+def random_id():
+    '''
+    Generate a random ID
+    '''
+    return random.randint(0,65536)
+
+def timestamp(hashed=False) -> str:
+    '''
+    Generate timestamp like 2019-04-17_11:27:07 or a hashed timestamp
+    '''
+    now = datetime.datetime.now()
+    value = now.strftime("%Y-%m-%d_%H:%M:%S")
+    if hashed:
+        value = hash_str(value)
+    return value
