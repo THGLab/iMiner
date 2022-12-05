67688878ce8dfa7840528149eec5f8267cb9a591
KSUN63 <62904517+KSUN63@users.noreply.github.com>
2022-12-05 13:50:06 -0800

    Update autodock4.py

--- iMiner/docking/autodock4.py
+++ iMiner/docking/autodock4.py
@@ -278,7 +278,7 @@ class AD4Docking(AutoDockBaseDocking):
         output_dir = Path(output_dir).resolve()
         ligand_best_pose = output_dir / "{}-best-pose.sdf".format(ligand_name)
         with open(ligand_best_pose, "w") as f1:
-            f1.write(all_sdf[num_run-1]
+            f1.write(all_sdf[num_run-1])
         
         return [ligand_name, smile, best_score, ligand_best_pose]
 
