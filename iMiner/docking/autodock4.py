b62efab63a6a87ee49950272e4beb218f30d338f
Oufan Zhang <oz57@berkeley.edu>
2023-01-02 15:58:57 -0800

    fix typo

--- iMiner/docking/autodock4.py
+++ iMiner/docking/autodock4.py
@@ -331,7 +331,7 @@ class AD4Docking(AutoDockBaseDocking):
         os.makedirs(output_dir, exist_ok=True)
         
         # process docking results 
-        dlgs = [self.result_path / (str(file.stem) + ".dlg") for file in ligands]
+        dlgs = [self.result_path / (str(file.stem) + ".dlg") for file in lig_outs]
         zipped_args = zip(dlgs, [output_dir] * len(dlgs))   
         counter = 0
         results = pd.DataFrame(columns=['original_name', 'smiles', 'score', 'path'])
