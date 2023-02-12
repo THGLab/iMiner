fix score averaging with nan

iMiner/docking/vina.py
@@ -162,12 +162,17 @@ class VinaDocking(AutoDockBaseDocking):
                 continue
             # save the ligand smiles
             ligand_smiles.append(self.convert_sdf_to_smiles(ligand))
-            # if contains invalid vina atom types
-            if not self.check_valid_atoms(ligand_smiles[-1]): 
-                # use 0 instead of nan, so that these molecules go into rl training
-                ligand_scores.append(0.)
-                ligand_conformation_paths.append("invalid atom types")
+            if ligand_smiles[-1] is None:
+                ligand_scores.append(np.nan)
+                ligand_conformation_paths.append("smiles sdf conversion error")
                 continue
+            
+            # if contains invalid vina atom types
+            #if not self.check_valid_atoms(ligand_smiles[-1]): 
+                # use 0 instead of nan, so that these molecules go into rl training
+            #    ligand_scores.append(0.)
+            #    ligand_conformation_paths.append("invalid atom types")
+            #    continue
             
             # execute vina docking under directory (for cpu: working directory, for gpu: binary directory)
             code, out, err = self._run_docking_under_folder(ligand_work_name, single_job_timeout) 

iMiner/rl_generate/evaluators/drug_likeliness.py
@@ -118,8 +118,8 @@ class DrugLikeliness():
             props = self.calc_props(input)
         except:
             return 0
-        if not self.check_valid_atomtypes(input):
-            return 0
+        #if not self.check_valid_atomtypes(input):
+        #    return 0
         log_prob = np.array([
             make_onehot(props[0], np.linspace(0,1,51)).dot(self.frac_csp3_LP),
             make_onehot(props[1], np.arange(10,61)).dot(self.heavy_atom_LP),

iMiner/rl_generate/evaluators/vina_local.py
@@ -25,6 +25,19 @@ def unpack_helper(func, args):
     '''
     return func(*args)
 
+def nan_average(x):
+    """
+    return the average of an array excluding the nan/zero entries; 
+    return nan if x is nan
+    """
+    if x is np.nan or x is 0:
+        return x
+    cx = np.nan_to_num(x)
+    nnan = (cx == 0).sum()
+    if nnan == 0:
+        return np.mean(x)
+    return cx.sum()/(x.shape[0] - nnan)
+
 def calc_average_df(df, column_name, key="smiles"):
     """
     calculate average of data column sharing the same key;
@@ -35,16 +48,14 @@ def calc_average_df(df, column_name, key="smiles"):
     dkey = sorted_df[key].values
     new_scores = []
     st = 0
-    i = 1
-    while i < df.shape[0]:
+    for i in range(1, df.shape[0]):
         if i == df.shape[0] - 1:
-            score = np.mean(data[st:])
+            score = nan_average(data[st:]) 
             new_scores += [score]*(i + 1 - st)
         elif dkey[i] != dkey[st]:
-            score = np.mean(data[st : i])
+            score = nan_average(data[st:i])
             new_scores += [score]*(i - st)
             st = i
-        i += 1
     sorted_df["adjusted_"+column_name] = new_scores
     return sorted_df.sort_index()
         