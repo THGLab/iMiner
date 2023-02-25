9a9cdafefc2bff2c615c9f629b11c24c1bb545e3
Yingze Wang <ericwangyz@pku.edu.cn>
2023-02-25 01:36:58 -0800

    make md example short

--- examples/md.py
+++ examples/md.py
@@ -6,7 +6,7 @@ from iMiner.core.md import MDProject
 
 
 if __name__ == "__main__":
-    proj = MDProject(project_name="helicase", project_path="./helicase")
+    proj = MDProject(project_name="helicase")
     proj.add_ligand("./helicase/UNC-0379-holo.sdf", name="UNC-0379-holo")
     proj.add_protein("./helicase/helicase-holo.pdb", name='helicase-holo')
     proj.read_params_json("./helicase/param.json")
