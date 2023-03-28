ea741bedb9a49ecf3792c517fb67d0947f51a428
KSUN63 <kysun@berkeley.edu>
2023-03-27 17:10:13 -0700

    minor fix

--- iMiner/docking/vina_flex.py
+++ iMiner/docking/vina_flex.py
@@ -20,7 +20,7 @@ import re
 import pandas as pd
 
 
-class VinaDocking(AutoDockBaseDocking):
+class VinaFlexDocking(AutoDockBaseDocking):
     def __init__(self, protein_pdb, docking_box, flex_res, temp_path: Optional[os.PathLike] = None, logger=None, **kwargs) -> None:
         """
         :param flex_res: list of residues that are kept flexible during the docking,
@@ -28,7 +28,6 @@ class VinaDocking(AutoDockBaseDocking):
         
         For right now, flexible docking could only take in protein with one chain. 
         """
-        
         super().__init__(protein_pdb, docking_box, logger=logger)
         self.working_path = Path(temp_path) / "{}-vina".format(self.protein_name)
         os.makedirs(self.working_path, exist_ok = True)
@@ -59,7 +58,7 @@ class VinaDocking(AutoDockBaseDocking):
 
         config_fp = self.working_path / "config.txt"
         lines = ["receptor = {}/{}_rigid.pdbqt".format(self.working_path, self.protein_name),
-                 "receptor = {}/{}_flex.pdbqt".format(self.working_path, self.protein_name),
+                 "flex = {}/{}_flex.pdbqt".format(self.working_path, self.protein_name),
                  "",
                  "center_x = {}".format((self.docking_box[0] + self.docking_box[3]) / 2),
                  "center_y = {}".format((self.docking_box[1] + self.docking_box[4]) / 2),
@@ -207,109 +206,6 @@ class VinaGPUDocking(VinaDocking):
         gpus = [None] * len(ligands)
         zipped_args = zip(ligands, [output_dir] * len(ligands), [single_job_timeout] * len(ligands), gpus)
         return zipped_args
-
-    def convert_sdf_to_pdbqt(self, sdf_path, output_path):
-        '''
-        Due to required input format for Vina-GPU, we need to convert sdf to pdbqt using Autodock Tools
-
-        :param sdf_path: str, path to the sdf file
-        :param output_path: str, path to the output pdbqt file
-
-        :return: True, if the run is successful
-        '''
-        # first convert sdf to mol2 using openbabel
-        random_code = random_id()
-        cmd_sdf_2_mol2 = "obabel -isdf {} -omol2 -O{}".format(sdf_path, self.working_path / f"{random_code}.mol2")
-        code, out, err = run_command(cmd_sdf_2_mol2, raise_error=False) 
-        if code != 0:
-            print(err)
-            return False
-        # then use Autodock Tools to convert mol2 to pdbqt
-        cmd_mol2_2_pdbqt = "{} {} -l {} -o {}".format(pythonsh_path, ligprep_path, self.working_path / f"{random_code}.mol2", output_path)
-        code, out, err = run_command(cmd_mol2_2_pdbqt, raise_error=False)
-        if code != 0:
-            print(err)
-            return False    
-        return True
-
-    def convert_adresult_to_sdf(self, adresult_path, output_path):
-        '''
-        Because Vina-GPU generated output results are not recognized by meeko, we need to convert adresult to sdf using openbabel
-
-        :param adresult_path: str, path to the adresult file
-        :param output_path: str, path to the output sdf file
-
-        :return: True, if the run is successful
-        '''
-        cmd = "obabel -ipdbqt {} -osdf -O{}".format(adresult_path, output_path)
-        code, out, err = run_command(cmd, raise_error=False) 
-        if code != 0:
-            print(err)
-            return False
-        return True
-
-    # def dock(self, ligands, output_dir):
-    #     '''
-    #     Run actual Autodock Vina GPU docking
-
-    #     :param ligands: list of ligands, each ligand is a path (str or os.PathLike) to the corresponding .sdf file
-    #     :param output_dir: str, path to the output directory where sdf files for the docked conformations will be saved
-    #     '''
-    #     # prepare lists to record results
-    #     ligand_smiles = []
-    #     ligand_scores = []
-    #     ligand_conformation_paths = []
-
-    #     # First make sure output_dir exists
-    #     os.makedirs(output_dir, exist_ok = True)
-
-    #     # convert output_dir to Path object
-    #     output_dir = Path(output_dir)
-
-    #     for ligand in ligands:
-    #         ligand_name = Path(ligand).stem
-    #         ligand_work_name = ligand_name + "_" + timestamp(hashed=True)
-    #         succ = self.convert_sdf_to_pdbqt(ligand, self.working_path / "{}.pdbqt".format(ligand_work_name))
-    #         if not (succ and os.path.exists(self.working_path / "{}.pdbqt".format(ligand_work_name))):
-    #             continue
-    #         # save the ligand smiles
-    #         ligand_smiles.append(self.convert_sdf_to_smiles(ligand))
-
-    #         # execute vina docking under the working directory
-    #         with set_directory(self.working_path):
-    #             cmd = f"{VINA_GPU_BINARY} --config config.txt --ligand {ligand_work_name}.pdbqt " + \
-    #                 "--out {ligand_work_name}_out.pdbqt"
-    #             code, out, err = run_command(cmd)
-
-    #         # obtain docking score from the results
-    #         energy = np.nan
-    #         strings = re.split('\n', out)
-    #         for line in strings:
-    #             if line[0:4] == '   1':
-    #                 energy = float(re.split(' +', line)[2])
-    #         ligand_scores.append(energy)
-
-    #         # save the conformation
-    #         # first check if the conformation is generated
-    #         if os.path.exists(self.working_path / f"{ligand_work_name}_out.pdbqt"):
-    #             # find the first available name
-    #             if os.path.exists(output_dir / f"{ligand_name}.sdf"):
-    #                 i = 1
-    #                 while os.path.exists(output_dir / f"{ligand_name}_{i}.sdf"):
-    #                     i += 1
-    #                 ligand_name = f"{ligand_name}_{i}"
-    #             succ = self.convert_adresult_to_sdf(self.working_path / "{}_out.pdbqt".format(ligand_work_name),
-    #                     output_dir / "{}.sdf".format(ligand_name))
-    #             if succ and os.path.exists(output_dir / "{}.sdf".format(ligand_name)):
-    #                 ligand_conformation_paths.append(str(output_dir / "{}.sdf".format(ligand_name)))
-    #             else:
-    #                 ligand_conformation_paths.append(None)
-    #         else:
-    #             ligand_conformation_paths.append(None)
-        
-    #     # generate the final pandas dataframe and return
-    #     df = pd.DataFrame({"smiles": ligand_smiles, "score": ligand_scores, "path": ligand_conformation_paths})
-    #     return df
 
 if __name__ == "__main__":
     print(VINA_BINARY)
