8e533f4ff4ba5abc664f8e1360b446218d745030
Kunyang Sun <62904517+KSUN63@users.noreply.github.com>
2023-06-05 12:28:29 -0700

    Update pathlib.py

--- iMiner/pathlib.py
+++ iMiner/pathlib.py
@@ -6,14 +6,14 @@ import os
 # (LAWRENCIUM)
 ADFR_install_path = '/global/scratch/users/jerry-li1996/src/ADFRsuite_x86_64Linux_1.0/bin'
 meeko_install_path = '/global/scratch/users/ozhang/env/iminer-rl/bin'
-iMiner_path = '/global/scratch/users/ozhang/covid/iMiner/iMiner'
+iMiner_path = '/global/scratch/users/ozhang/covid/iMiner'
 ad4gpu_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/autodock_gpu_128wi'
 
 
 # savio
 # ADFR_install_path = '/global/home/groups/co_armada2/local/ADFRsuite/bin'
 # meeko_install_path = '/global/home/groups/co_armada2/conda_envs/vina/bin'
-# iMiner_path = '/global/home/users/kysun/iMiner-github/iMiner'
+# iMiner_path = '/global/home/users/kysun/iMiner'
 # ad4gpu_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/autodock_gpu_128wi'
 # ad4gpu_analysis_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/adgpu_analysis'
 # flexrec_prep_path = f'{ADFR_install_path}/prepare_flexreceptor.py'
@@ -25,9 +25,9 @@ meeko_ligconv_path = f"{meeko_install_path}/mk_export.py"
 
 autogrid_path = f'{ADFR_install_path}/autogrid4'
 
-VINA_BINARY = Path(iMiner_path) / 'docking/bins/vina'
-VINA_GPU_SCRIPT = Path(iMiner_path) / 'docking/run_vina_gpu.sh'
-VINA_GPU_BINARY_PATH = Path(iMiner_path) / "docking/bins"
+VINA_BINARY = Path(iMiner_path) / 'iMiner/docking/bins/vina'
+VINA_GPU_SCRIPT = Path(iMiner_path) / 'iMiner/docking/run_vina_gpu.sh'
+VINA_GPU_BINARY_PATH = Path(iMiner_path) / "iMiner/docking/bins"
 
 # tankbind_dir_path = os.path.dirname(tankbind_python_path)
 # tankbind_python_path = '/home/jerry/anaconda3/envs/tankbind/bin/python'
