e9405ed40fe3100305fd328afc4fb9f465894c46
KSUN63 <kysun@berkeley.edu>
2023-06-05 10:30:14 -0700

    update path

--- iMiner/pathlib.py
+++ iMiner/pathlib.py
@@ -5,7 +5,7 @@ import os
 
 ADFR_install_path = '/global/home/groups/co_armada2/local/ADFRsuite/bin'
 meeko_install_path = '/global/home/groups/co_armada2/conda_envs/vina/bin'
-iMiner_path = '/global/home/users/kysun/iMiner-github'
+iMiner_path = '/global/home/users/kysun/iMiner-github/iMiner'
 ad4gpu_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/autodock_gpu_128wi'
 ad4gpu_analysis_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/adgpu_analysis'
 
