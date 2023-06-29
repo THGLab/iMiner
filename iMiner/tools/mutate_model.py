5db6b5557ecc74cc261f4bb4e5b9a344777ad2b2
KSUN63 <kysun@berkeley.edu>
2023-06-28 23:26:29 -0700

    finished testing mutagenesis block

--- iMiner/tools/mutate_model.py
+++ iMiner/tools/mutate_model.py
@@ -10,29 +10,12 @@ The code is modified by iMiner developers and is licensed under the MIT license.
 Date of modification: 06/26/2023
 """
 
-import sys
 import os
+from pathlib import Path
 
 from modeller import *
 from modeller.optimizers import MolecularDynamics, ConjugateGradients
 from modeller.automodel import autosched
-
-#
-#  mutate_model.py
-#
-#     Usage:   python mutate_model.py modelname respos resname chain > logfile
-#
-#     Example: python mutate_model.py 1t29 1699 LEU A > 1t29.log
-#
-#
-#  Creates a single in silico point mutation to sidechain type and at residue position
-#  input by the user, in the structure whose file is modelname.pdb
-#  The conformation of the mutant sidechain is optimized by conjugate gradient and
-#  refined using some MD.
-#
-#  Note: if the model has no chain identifier, specify "" for the chain argument.
-#
-
 
 def optimize(atmsel, sched):
     #conjugate gradient
@@ -71,116 +54,124 @@ def make_restraints(mdl1, aln):
                 spline_dx=0.3, spline_min_points = 5, aln=aln,
                 spline_on_site=True)
 
-#first argument
-modelname, respos, restyp, chain, = sys.argv[1:]
-
 triplet_to_singlet = {
-    'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E',
-    'PHE': 'F', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
-    'LYS': 'K', 'LEU': 'L', 'MET': 'M', 'ASN': 'N',
-    'PRO': 'P', 'GLN': 'Q', 'ARG': 'R', 'SER': 'S',
-    'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y'
+    'ALA': 'A',
+    'ARG': 'R',
+    'ASN': 'N',
+    'ASP': 'D',
+    'CYS': 'C',
+    'GLN': 'Q',
+    'GLU': 'E',
+    'GLY': 'G',
+    'HIS': 'H',
+    'ILE': 'I',
+    'LEU': 'L',
+    'LYS': 'K',
+    'MET': 'M',
+    'PHE': 'F',
+    'PRO': 'P',
+    'SER': 'S',
+    'THR': 'T',
+    'TRP': 'W',
+    'TYR': 'Y',
+    'VAL': 'V'
 }
 
-log.verbose()
+class Mutagenesis:
+    """
+    mutagenesis class that takes in a pdb file and a residue position and
+    mutates the residue to the desired type    
+    """
+    def __init__(self, pdb_file, temp_path):
+        self.pdb_file = pdb_file
+        self.protein_name = Path(pdb_file).stem
+        self.temp_path = Path(temp_path)
+        self.temp_path.mkdir(parents=True, exist_ok=True)
+        
 
-# Set a different value for rand_seed to get a different final model
-env = Environ(rand_seed=-49837)
+    def mutate(self, chain, respos, resname, radius = 10):
+        """
+        mutates the residue in the pdb file and saves the mutated pdb file
 
-env.io.hetatm = True
-#soft sphere potential
-env.edat.dynamic_sphere=False
-#lennard-jones potential (more accurate)
-env.edat.dynamic_lennard=True
-env.edat.contact_shell = 4.0
-env.edat.update_dynamic = 0.39
+        Args:
+            mutation_list (list): list of mutations to be made in the order of chain, residue position, 
+            residue name to be mutated.
+        """
+        env = Environ()
+        env.io.hetatm = True
+        #soft sphere potential
+        env.edat.dynamic_sphere=False
+        #lennard-jones potential (more accurate)
+        env.edat.dynamic_lennard=True
+        env.edat.contact_shell = 4.0
+        env.edat.update_dynamic = 0.39
 
-# Read customized topology file with phosphoserines (or standard one)
-env.libs.topology.read(file='$(LIB)/top_heav.lib')
+        env.io.atom_files_directory = ['.', '../atom_files']
+        env.libs.topology.read(file='$(LIB)/top_heav.lib')
+        env.libs.parameters.read(file='$(LIB)/par.lib')
 
-# Read customized CHARMM parameter library with phosphoserines (or standard one)
-env.libs.parameters.read(file='$(LIB)/par.lib')
+        mdl1 = Model(env, file=self.pdb_file)
+        aln = Alignment(env)
+        aln.append_model(mdl1, atom_files=self.protein_name, align_codes=self.protein_name)
+
+        orginal_aa = triplet_to_singlet[mdl1.chains[chain].residues[str(respos)].name]
+        print("*"*50)
+        print("original aa: ", orginal_aa)
+        mutated_aa = triplet_to_singlet[resname]
+        
+        # mutate residue types
+        s = Selection(mdl1.chains[chain].residues[str(respos)])
+        s.mutate(residue_type=resname)
+        
+        mutation_info = '_' + chain + '_' + orginal_aa + str(respos) + mutated_aa
+        aln.append_model(mdl1, align_codes=self.protein_name)
+
+        mdl1.clear_topology()
+        mdl1.generate_topology(aln[-1])
+        mdl1.transfer_xyz(aln)
+
+        # Build the remaining unknown coordinates
+        mdl1.build(initialize_xyz=False, build_method='INTERNAL_COORDINATES')
+        mdl2 = Model(env, file=self.pdb_file)
+        mdl1.res_num_from(mdl2,aln)
+
+        #It is usually necessary to write the mutated sequence out and read it in
+        #before proceeding, because not all sequence related information about MODEL
+        #is changed by this command (e.g., internal coordinates, charges, and atom
+        #types and radii are not updated).
+        mdl1.write(file=self.protein_name+mutation_info+'.tmp')
+        mdl1.read(file=self.protein_name+mutation_info+'.tmp')
 
 
-# Read the original PDB file and copy its sequence to the alignment array:
-mdl1 = Model(env, file=modelname)
-ali = Alignment(env)
-ali.append_model(mdl1, atom_files=modelname, align_codes=modelname)
+        make_restraints(mdl1, aln)
+        mdl1.env.edat.nonbonded_sel_atoms=1
+        sched = autosched.loop.make_for_model(mdl1)
 
-#set up the mutate residue selection segment
-s = Selection(mdl1.chains[chain].residues[respos])
-orginal_aa = triplet_to_singlet[mdl1.chains[chain].residues[respos].name]
-mutated_aa = triplet_to_singlet[restyp]
+        #only optimize the selected residue (in first pass, just atoms in selected
+        #residue, in second pass, include nonbonded neighboring atoms)
+        #set up the mutate residue selection segment
+        s = Selection(mdl1.chains[chain].residues[str(respos)].atoms['CA'].select_sphere(radius))
 
-#perform the mutate residue operation
-s.mutate(residue_type=restyp)
-#get two copies of the sequence.  A modeller trick to get things set up
-ali.append_model(mdl1, align_codes=modelname)
+        mdl1.restraints.unpick_all()
+        mdl1.restraints.pick(s)
 
-# Generate molecular topology for mutant
-mdl1.clear_topology()
-mdl1.generate_topology(ali[-1])
+        s.energy()
+        s.randomize_xyz(deviation=4.0)
 
+        mdl1.env.edat.nonbonded_sel_atoms=2
+        optimize(s, sched)
+        mdl1.env.edat.nonbonded_sel_atoms=1
+        optimize(s, sched)
 
-# Transfer all the coordinates you can from the template native structure
-# to the mutant (this works even if the order of atoms in the native PDB
-# file is not standard):
-#here we are generating the model by reading the template coordinates
-mdl1.transfer_xyz(ali)
+        s.energy()
+        output_fp = self.temp_path / (self.protein_name + mutation_info + '.pdb')
+        mdl1.write(file=str(output_fp))
 
-# Build the remaining unknown coordinates
-mdl1.build(initialize_xyz=False, build_method='INTERNAL_COORDINATES')
+        #delete the temporary file
+        os.remove(self.protein_name+mutation_info+'.tmp')
+        return True
+    
+if __name__ == '__main__':
 
-#yes model2 is the same file as model1.  It's a modeller trick.
-mdl2 = Model(env, file=modelname)
-
-#required to do a transfer_res_numb
-#ali.append_model(mdl2, atom_files=modelname, align_codes=modelname)
-#transfers from "model 2" to "model 1"
-mdl1.res_num_from(mdl2,ali)
-
-#It is usually necessary to write the mutated sequence out and read it in
-#before proceeding, because not all sequence related information about MODEL
-#is changed by this command (e.g., internal coordinates, charges, and atom
-#types and radii are not updated).
-
-mdl1.write(file=modelname+restyp+respos+'.tmp')
-mdl1.read(file=modelname+restyp+respos+'.tmp')
-
-#set up restraints before computing energy
-#we do this a second time because the model has been written out and read in,
-#clearing the previously set restraints
-make_restraints(mdl1, ali)
-
-#a non-bonded pair has to have at least as many selected atoms
-mdl1.env.edat.nonbonded_sel_atoms=1
-
-sched = autosched.loop.make_for_model(mdl1)
-
-#only optimize the selected residue (in first pass, just atoms in selected
-#residue, in second pass, include nonbonded neighboring atoms)
-#set up the mutate residue selection segment
-s = Selection(mdl1.chains[chain].residues[respos].atoms['CA'].select_sphere(8))
-
-mdl1.restraints.unpick_all()
-mdl1.restraints.pick(s)
-
-s.energy()
-
-s.randomize_xyz(deviation=4.0)
-
-mdl1.env.edat.nonbonded_sel_atoms=2
-optimize(s, sched)
-
-#feels environment (energy computed on pairs that have at least one member
-#in the selected)
-mdl1.env.edat.nonbonded_sel_atoms=1
-optimize(s, sched)
-
-s.energy()
-
-#give a proper name
-mdl1.write(file="./mutated_protein_cdd1845/"+modelname+'_'+orginal_aa+respos+mutated_aa+'.pdb')
-
-#delete the temporary file
-os.remove(modelname+restyp+respos+'.tmp')
+    mutagenesis = Mutagenesis("../../examples/data/sars_cov_2_helicase.pdb", "./data")
+    mutagenesis.mutate("A", 179, "GLN")
