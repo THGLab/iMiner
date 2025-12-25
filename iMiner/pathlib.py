7b7a9389963928cf0538ebdc16253b5e61c53cfc
Joe Cavanagh <84742469+jmcavanagh@users.noreply.github.com>
2025-12-25 10:22:28 -0500

    Refactor pathlib.py by cleaning up system paths
    Removed commented-out paths for different systems and updated iMiner_path to use the current file's directory.

--- iMiner/pathlib.py
+++ iMiner/pathlib.py
@@ -1,20 +1,11 @@
 from pathlib import Path
 import os
-
-# CHANGE THESE UNDER DIFFERENT SYSTEMS
+iMiner_path = Path(__file__).parent
 
 # (LAWRENCIUM)
 ADFR_install_path = '/global/scratch/users/jerry-li1996/src/ADFRsuite_x86_64Linux_1.0/bin'
 meeko_install_path = '/global/scratch/users/ozhang/env/iminer-rl/bin'
-iMiner_path = '/global/scratch/users/ozhang/covid/iMiner/iMiner'
 ad4gpu_path = '/global/scratch/users/jerry-li1996/bins/AutoDock-GPU/bin/autodock_gpu_128wi'
-
-# (SAVIO)
-# ADFR_install_path = '/global/home/groups/co_armada2/local/ADFRsuite/bin'
-# meeko_install_path = '/global/home/groups/co_armada2/conda_envs/vina/bin'
-# iMiner_path = '/global/home/groups/co_armada2/avidd/iMiner/iMiner'
-# ad4gpu_path = '/global/home/groups/co_armada2/local/AutoDock-GPU/bin/autodock_gpu_128wi'
-######################################
 
 protein_prep_path = f'{ADFR_install_path}/prepare_receptor'
 meeko_ligprep_path = f"{meeko_install_path}/mk_prepare_ligand.py"
